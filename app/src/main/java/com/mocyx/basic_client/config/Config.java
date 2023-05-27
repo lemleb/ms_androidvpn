f876a5c9202719392979419ba5802ea144f87957
Béla Lemle <bela.lemle@student.tugraz.at>
2023-05-27 22:58:38 +0200

    [FIRST COMMIT] base project

---
+++ app/src/main/java/com/mocyx/basic_client/config/Config.java
@@ -0,0 +1,19 @@
+package com.mocyx.basic_client.config;
+
+public class Config {
+    //只代理本app的流量
+    public static boolean testLocal = false;
+    //配置dns
+    public static String dns = "1.1.1.1";
+    //io日志
+    @Deprecated
+    public static boolean logRW = false;
+    //ack日志
+    @Deprecated
+    public static boolean logAck = false;
+}
+
+
+
+
+
